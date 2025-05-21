bb8f87d9746af3edbdf55eede019ca6fbc520339
Waseemuddin Shaikh <waseem.uddin@live.com>
2025-05-21 20:03:36 +0500

    updated version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App::</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 2.1</p>');
 })
 
 app.get('/products', (req, res) => {
@@ -19,7 +19,7 @@ app.get('/products', (req, res) => {
   ])
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Demo app is up and listening to port: ${port}`);
 })
- 
+
